33ecdab91836b1a2394a3ab78b9b1417d69be38e
zowhair <zowhair@gmail.com>
2022-02-15 02:04:56 +0500

    Added users data

--- app.js
+++ app.js
@@ -4,13 +4,42 @@ const app = express();
 let PORT = 3000;
 
 
+let users = [
+  {
+    "id": 1,
+    "username": "asim",
+    "role": "teacher"
+  },
+  {
+    "id": 2,
+    "username": "zakir",
+    "role": "student"
+  },
+  {
+    "id": 3,
+    "username": "liaqt",
+    "role": "student"
+  }
+];
+
 app.get('/', (req, res) => {
 	res.json({title: 'Our new project started'})
 })
 
-app.get('/name', (req, res) => {
-	res.json({name: 'zakir and liaqat'})
+app.get('/users', (req, res)=> {
+	res.json(users)
 })
+
+app.get('/users/:id', (req, res) => {
+	const userId = Number(req.params.id);
+	const getUser = users.find((user) => user.id === userId);
+
+	if(!getUser) {
+		res.status(500).send('user not found');
+	}else {
+		res.json(getUser);
+	}
+});
 
 app.get('/home', (req, res) => {
 	res.end('This is home page');
